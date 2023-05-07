069a04b5f4ce3da11e794cd49fb39ef40233add4
Łukasz Kuk <lukasz010420@gmail.com>
2023-05-07 11:31:53 +0200

    oop

--- main.py
+++ main.py
@@ -4,46 +4,50 @@ from selectorlib import Extractor
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
-connection = sqlite3.connect("data.db")
+
+class Event:
+    @staticmethod
+    def scrape(url):
+        """Scrape the page source from the URL"""
+        response = requests.get(url)
+        source = response.text
+        return source
+
+    @staticmethod
+    def extract(source):
+        extractor = Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url)
-    source = response.text
-    return source
+class Database:
+    def __init__(self):
+        self.connection = sqlite3.connect("data.db")
 
+    def store(self, text):
+        row = text.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("insert into events values(?, ?, ?)", row)
+        self.connection.commit()
 
-def extract(source):
-    extractor = Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
-
-
-def store(text):
-    row = text.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("insert into events values(?, ?, ?)", row)
-    connection.commit()
-
-
-def read(text):
-    row = text.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("select * from events where band=? and city=? and date=?", (band, city, date))
-    rows = cursor.fetchall()
-    return rows
+    def read(self, text):
+        row = text.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("select * from events where band=? and city=? and date=?", (band, city, date))
+        rows = cursor.fetchall()
+        return rows
 
 
 if __name__ == "__main__":
-    source = scrape(URL)
-    extracted = extract(source)
+    source = Event.scrape(URL)
+    extracted = Event.extract(source)
     print(extracted)
 
     if extracted != "No upcoming tours":
-        row = read(extracted)
+        db = Database()
+        row = db.read(extracted)
         if not row:
-            store(extracted)
+            db.store(extracted)
